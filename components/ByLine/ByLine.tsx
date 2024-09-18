7c3b3f23e19a04dc249eafe9835c534ee07c7661
Stephen Taylor <stephentaylor950@yahoo.com>
2024-09-18 17:07:38 +1000

    Add types to parameters of component-function ByLine.

--- components/ByLine/ByLine.tsx
+++ components/ByLine/ByLine.tsx
@@ -1,6 +1,6 @@
 import styles from "./ByLine.module.css";
 
-export default function ByLine( {heading, altText, sizes, srcset, providerName} ) {
+export default function ByLine( {heading, altText, sizes, srcset, providerName}: {heading: string, altText: string, sizes: string, srcset: string, providerName: string} ) {
   return (
     <div>
       <p className={styles.bylineLabel}>{heading}</p>
@@ -9,7 +9,7 @@ export default function ByLine( {heading, altText, sizes, srcset, providerName} 
           <a>
             <span>
               {/* <img alt="ABC Radio National" sizes="48px" srcset="https://live-production.wcms.abc-cdn.net.au/b72e54c25b40409156c20bc8bf223c27?impolicy=wcms_crop_resize&cropH=1500&cropW=1500&xPos=0&yPos=0&width=160&height=160 160w, https://live-production.wcms.abc-cdn.net.au/b72e54c25b40409156c20bc8bf223c27?impolicy=wcms_crop_resize&cropH=1500&cropW=1500&xPos=0&yPos=0&width=862&height=862 862w" src="https://live-production.wcms.abc-cdn.net.au/b72e54c25b40409156c20bc8bf223c27?impolicy=wcms_crop_resize&cropH=1500&cropW=1500&xPos=0&yPos=0&width=862&height=862" loading="eager" data-component="Image" ratio="1x1"/> */}
-              <img alt={altText} sizes={sizes} srcSet={srcset} loading="eager" data-component="Image" ratio="1x1"/>
+              <img alt={altText} sizes={sizes} srcSet={srcset} loading="eager" data-component="Image"/>
             </span>
             <span>
               {providerName}
