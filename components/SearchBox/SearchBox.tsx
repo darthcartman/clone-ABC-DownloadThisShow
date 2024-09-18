9c5eaac98f3c453565641c628c13767556c56333
Stephen Taylor <stephentaylor950@yahoo.com>
2024-09-18 21:22:41 +1000

    Remove 'maxLength' property from <input> in components/SearchBox/SearchBox.tsx.

--- components/SearchBox/SearchBox.tsx
+++ components/SearchBox/SearchBox.tsx
@@ -9,7 +9,7 @@ export default function SearchBox() {
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" width="1em" height="1em" data-component="Search" className={styles.searchInputIcon} aria-hidden="true" data-testid="input-search-icon">
               <path stroke="currentColor" strokeWidth="2" d="m21 21-6.904-6.904m0 0a6.5 6.5 0 1 0-9.192-9.192 6.5 6.5 0 0 0 9.192 9.192Z"></path>
             </svg>
-            <input className={styles.searchBoxInputElement} id="abc-search-input" autoComplete="off" autoCorrect="off" autoCapitalize="off" enterKeyHint="search" placeholder="Search..." maxLength="512" type="search" role="combobox"/>
+            <input className={styles.searchBoxInputElement} id="abc-search-input" autoComplete="off" autoCorrect="off" autoCapitalize="off" enterKeyHint="search" placeholder="Search..." type="search" role="combobox"/>
           </div>
         </form>
       </div>
