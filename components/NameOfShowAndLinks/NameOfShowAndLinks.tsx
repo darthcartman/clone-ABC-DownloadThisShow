c64be5ca8281372176c94314f4411c4e11b5ccd7
Stephen Taylor <stephentaylor950@yahoo.com>
2024-09-18 21:09:18 +1000

    Replace 'class' with 'className' in components/NameOfShowAndLinks/NameOfShowAndLinks.tsx.

--- components/NameOfShowAndLinks/NameOfShowAndLinks.tsx
+++ components/NameOfShowAndLinks/NameOfShowAndLinks.tsx
@@ -11,7 +11,7 @@ export default function NameOfShowAndLinks() {
       </h1>
       <div className={styles.buttonsWrapper}>
         <a className={styles.buttonBelowShowTitle}>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" width="1em" height="1em" data-component="Series" class={styles.icon}>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" width="1em" height="1em" data-component="Series" className={styles.icon}>
             <rect width="12" height="8" x="3" y="5" stroke="currentColor" strokeWidth="2" rx="0.5"></rect>
             <path stroke="currentColor" strokeWidth="2" d="M6 16h11.5a.5.5 0 0 0 .5-.5V8"></path>
             <path stroke="currentColor" strokeWidth="2" d="M9 19h11.5a.5.5 0 0 0 .5-.5V11"></path>
@@ -19,7 +19,7 @@ export default function NameOfShowAndLinks() {
           All Episodes
         </a>
         <a className={styles.buttonBelowShowTitle}>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" width="1em" height="1em" data-component="Mail" class="">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" width="1em" height="1em" data-component="Mail" className={styles.icon}>
             <path stroke="currentColor" stroke-width="2" d="M20.5 5h-17a.5.5 0 0 0-.5.5v13a.5.5 0 0 0 .5.5h17a.5.5 0 0 0 .5-.5v-13a.5.5 0 0 0-.5-.5Z"></path>
             <path stroke="currentColor" stroke-width="2" d="m21 6.5-8.74 5.34a.5.5 0 0 1-.52 0L3 6.5"></path>
           </svg>
